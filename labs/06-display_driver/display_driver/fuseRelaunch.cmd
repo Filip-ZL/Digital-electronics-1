c55abbd77a83c2c8fa462f24c25e4f9db849d51b
Filip-ZL <58042992+Filip-ZL@users.noreply.github.com>
2020-03-12 22:42:17 +0100

    Add files via upload

---
+++ labs/06-display_driver/display_driver/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "D:/VHDL/Projekt/Labs06-display_driver/display_driver/driver_7segg_isim_beh.exe" -prj "D:/VHDL/Projekt/Labs06-display_driver/display_driver/driver_7segg_beh.prj" "work.driver_7segg" 
